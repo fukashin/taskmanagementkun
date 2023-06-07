4afa69069034675f178a2f0d0ad0ffe7f50a842d
Admin フカシン <sinfuk1997@gmail.com>
2023-06-07 16:35:13 +0900

    URL変更

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,8 +5,8 @@ import { Task2} from '../domain/models/models';
 import { AxiosResponse } from 'axios';
 
 
-//const API_BASE_URL = 'http://localhost:8000';
-const API_BASE_URL = 'http://172.31.3.68:8000';
+const API_BASE_URL = 'http://localhost:8000';
+//const API_BASE_URL = 'http://あなたのec2のパブリックIPアドレス:8000';
 
 
 export const createTask = async (name:string ,current_time:Date,due_date:Date ,creator:string, assignee:string, status:string,importance:string,kind:string) => {
